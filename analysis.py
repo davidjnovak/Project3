2d0a7d7f130280c55c6efc292b794422f1747f17
greggriffin2 <greg.griffin2@gmail.com>
2021-10-25 12:11:13 -0400

    added author names

--- analysis.py
+++ analysis.py
@@ -1,5 +1,7 @@
 # analysis.py
 # -----------
+# Greg Griffin and David Novak
+# -----------
 # Licensing Information:  You are free to use or extend these projects for
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
